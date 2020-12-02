4299cade0ca890a2bfce62633e8cd9748689b7af
Fabricio <fabri_rozen@hotmail.com>
2020-12-02 19:52:54 -0300

    Update fixes2

--- controllers/altausuarios.php
+++ controllers/altausuarios.php
@@ -7,24 +7,28 @@ require '../models/Usuarios.php';
 require '../views/registrarse.php';
 require '../views/AltaUsuarioOk.php';
 
-if (count($_POST)>0){
-	$us = new Usuarios;
 
-	if(!isset($_POST['Nombre'])) throw("error altausuario 1");
-	if(!isset($_POST['Apellido'])) throw ("error altausuario 2");
-	if(!isset($_POST['Usuario'])) throw ("error altausuario 3");
-	if(!isset($_POST['DNI'])) throw ("error altausuario 4");
-	if(!isset($_POST['Telefono'])) throw ("error altausuario 5");
-	if(!isset($_POST['Direccion'])) throw ("error altausuario 6");
-	if(!isset($_POST['Contrasenia'])) throw ("error altausuario 7");
-	if(!isset($_POST['Contrasenia2'])) throw ("error altausuario 8");
+	if(isset($_POST['Contrasenia']) and isset($_POST['Contrasenia2']) and $_POST['Usuario'])
+	{
+		if($_POST['Contrasenia'] != '' and $_POST['Contrasenia2'] != '' and $_POST['Usuario'] != ''){
+			$us = new Usuarios;
 
-	$us->crearUsuario($_POST['Nombre'], $_POST['Apellido'], $_POST['Usuario'], $_POST['DNI'], $_POST['Telefono'], $_POST['Direccion'], $_POST['Contrasenia'], $_POST['Contrasenia2'] );
+		if(!isset($_POST['Nombre'])) throw("error altausuario 1");
+		if(!isset($_POST['Apellido'])) throw ("error altausuario 2");
+		if(!isset($_POST['DNI'])) throw ("error altausuario 3");
+		if(!isset($_POST['Telefono'])) throw ("error altausuario 4");
+		if(!isset($_POST['Direccion'])) throw ("error altausuario 5");
 
-	$v = new AltaUsuarioOk();
+		$us->crearUsuario($_POST['Nombre'], $_POST['Apellido'], $_POST['Usuario'], $_POST['DNI'], $_POST['Telefono'], $_POST['Direccion'], $_POST['Contrasenia'], $_POST['Contrasenia2'] );
+			
+		$v = new AltaUsuarioOk();
+		}
+		
 
-}else{
-	$v = new registrarse();
-}
+	}
+	else{
+		$v = new registrarse();
+	}
+
 
 $v->render();
--- html/registrarse.php
+++ html/registrarse.php
@@ -11,7 +11,7 @@
 
 	<div id="error"></div>
 
-	<div id="formu" class="dflex justify-content-center align-items-center text-center"> 
+	<div id="formu" class="text-center"> 
 		<form action="" method="post">
 			<div class="form-group">
 				<label for="Nom">Nombre</label>
@@ -53,7 +53,7 @@
 
 </body>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 		document.getElementById("formu").onsubmit = function () {
 			var cont = document.getElementById("con1").value;
 			var cont2 = document.getElementById("con2").value;
@@ -92,7 +92,7 @@
 				return false;
 			};
 		};
-	</script>
+	</script> -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
